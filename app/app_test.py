921fd8c7d3aff11ff12a6581bd92e36d9708862b
Guillaume Lazaro <guillaume.lazaro@webinage.fr>
2021-02-05 14:53:27 +0100

    hello BG

--- app/app_test.py
+++ app/app_test.py
@@ -4,11 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 #route for home page
-@app.route("/")
-def index():
-    return "Hello AP Formation 🤖 ! \n\nCeci est une micro application python faite avec Flask 🐍 \n\n"
+# @app.route("/")
+# def index():
+#     return "Hello AP Formation 🤖 ! \n\nCeci est une micro application python faite avec Flask 🐍 \n\n"
 
+@app.route('/')
+def helloBg():
+    return 'hello BG'
+    
 if __name__ == "__main__":
+    # print(index())
     app.run(debug=True, port=5000)
-
 
